73be73726e9938011d3c7504275bd2a178eaad1c
Simon Holroyd <sholroyd@gmail.com>
2013-06-28 10:58:13 -0400

    Adding core functionality

--- lib/git_team_stats.rb
+++ lib/git_team_stats.rb
@@ -2,8 +2,137 @@ require "git_team_stats/version"
 require "git_team_stats/git_parse"
 
 module GitTeamStats
-  
 
+  @project = nil
+
+  @inspected = false
+
+  def self.start(project)
+    @project = project
+    
+    @commits_in_detail = []
+
+    @project[:parsers] = []
+    
+    @project[:repos].each do |repo|
+      @project[:parsers].push(
+        GitParse.new(repo)
+      )
+    end
+
+  end
+
+  def self.count_commits
+    count = 0
+    
+    @project[:parsers].each do |parser|
+      count += parser.count_commits()
+    end
+
+    return count
+  end
+
+  def self.collect_commits
+    unless @inspected
+
+      @project[:parsers].each do |parser|
+        parser.get_commits()
+      end
+    end
+  end
+
+  def self.inspect_commits
+    unless @inspected
+
+      self.collect_commits()
+
+      @project[:parsers].each do |parser|
+        @commits_in_detail += parser.get_commit_details()
+      end
+
+      @inspected = true
+    end
+  end
+
+  def self.team_cumulative_stats
+    self.inspect_commits()
+    
+    team_cumulative = {
+      :commits => 0,
+      :lines => 0,
+      :edits => 0,
+      :file_types => {}
+    }
+    @commits_in_detail.each do |commit|
+      team_cumulative[:commits] += 1
+      team_cumulative[:lines] += commit[:lines]
+      team_cumulative[:edits] += commit[:edits]
+
+      commit[:file_types].each do |language, details|
+        if team_cumulative[:file_types].key? language
+          team_cumulative[:file_types][language][:lines] += details[:lines]
+          team_cumulative[:file_types][language][:edits] += details[:edits]
+        else
+          team_cumulative[:file_types][language] = {
+            :lines => details[:lines],
+            :edits => details[:edits]
+          }
+        end
+      end
+    end
+
+    return team_cumulative
+  end
+
+  def self.user_cumulative_stats(user)
+    self.inspect_commits()
+    
+    user_cumulative = {
+      :commits => 0,
+      :lines => 0,
+      :edits => 0,
+      :file_types => {}
+    }
+    @commits_in_detail.each do |commit|
+      if user[:aliases].any?{ |name| name == commit[:committer] }
+        user_cumulative[:commits] += 1
+        user_cumulative[:lines] += commit[:lines]
+        user_cumulative[:edits] += commit[:edits]
+
+        commit[:file_types].each do |language, details|
+          if user_cumulative[:file_types].key? language
+            user_cumulative[:file_types][language][:lines] += details[:lines]
+            user_cumulative[:file_types][language][:edits] += details[:edits]
+          else
+            user_cumulative[:file_types][language] = {
+              :lines => details[:lines],
+              :edits => details[:edits]
+            }
+          end
+        end
+      end
+    end
+
+    return user_cumulative
+  end
+
+  def self.get_cache_output
+    return @commits_in_detail
+  end
+
+  def self.load_from_cache(data)
+    @commits_in_detail = data
+    @inspected = true
+  end
+
+  def self.get_cache_file_name
+    hash_str = ""
+    @project[:parsers].each{ |parser|
+      hash_str += parser.get_head_short_hash().to_s + "-"
+    }
+
+    return $project[:name].to_s + "-" + hash_str + @project[:start_date].to_s + '-' + @project[:end_date].to_s
+  end
 
 
 end
--- lib/git_team_stats/git_parse.rb
+++ lib/git_team_stats/git_parse.rb
@@ -1,11 +1,197 @@
-class GitTeamStats::GitParse
+require 'progress_bar'
+require 'rainbow'
+require 'language_sniffer'
+
+class GitParse
+
 
   def initialize(directory)
+    @commits = []
+
+    @ignored_directories = []
+    @start_date = nil
+    @end_date = nil
+    
     @directory = directory
   end
 
-  
-  
 
-  
+  def execute(command, directory = ".")
+    directory = `cd #{directory}; #{command}`
+  end
+
+
+  def get_commits
+
+    hash = ""
+
+    puts "finding commits in #{@directory}".color(:yellow)
+    rev_list = get_rev_list()
+    bar = ProgressBar.new(rev_list.lines.length, :bar, :percentage, :eta);
+    rev_list.lines.each do |line|
+      bar.increment!
+      if ( line =~ /^commit ([\w]+)/)
+          hash = $1
+          next
+      else hash != ""
+          if ( line =~ /^([\d]+)\s(.+)\s<(.+)>$/ )
+              timestamp = $1.to_i
+              committer = $2
+              email = $3
+          end
+      
+          @commits.push({
+              :hash => hash,
+              :committer => committer,
+              :path => @directory,
+              :timestamp => timestamp,
+          })
+
+          hash = ""
+      end
+    end
+
+    return @commits
+  end
+
+
+
+  def count_commits
+    if @commits.empty?
+      get_commits()
+    end
+
+    return @commits.count
+  end
+
+  def get_authors
+    users = []
+    get_authors_from_shortlog().lines.each do |line|
+      if !users.any? {|user| user[:name] = line }
+        users.push({
+          :name => line.strip,
+          :aliases => [line.strip]
+        })
+      end
+    end
+    return users
+  end
+
+  def get_authors_from_shortlog
+    return execute("git shortlog -s | cut -c8-", @directory)
+  end
+
+  def get_head_short_hash
+    return execute("git rev-parse --short HEAD", @directory).strip
+  end
+
+  def get_rev_list
+    date_str = ""
+    if @start_date != nil
+      date_str += "--max-age=%s " % [@start_date.to_i.to_s]
+    end
+    if @end_date != nil
+      date_str += "--min-age=%s " % [@end_date.to_i.to_s]
+    end
+    return execute("git rev-list --reverse --pretty=format:\"%%at %%aN <%%aE>\" %s HEAD" % date_str, @directory)
+  end
+
+  def get_diff_tree(commit_hash)
+    return execute("git diff-tree %s --numstat" % commit_hash, @directory)
+  end
+
+  def get_file(commit_hash, file_name)
+    return execute("git show %s:%s 2>&1" % [commit_hash, file_name], @directory)
+  end
+
+  def parse_diff_tree(commit_hash)
+
+    total_lines = 0
+    total_edits = 0
+    file_types = {}
+
+    diff_tree = get_diff_tree(commit_hash)
+
+    diff_tree.lines.each do |line|
+      
+      if ( line =~ /^([\d]+)\s([\d]+)(.*[^\w](\w+))$/)
+
+        insertions = $1.to_i
+        deletions = $2.to_i
+
+        lines = insertions - deletions
+        edits = insertions + deletions
+
+        file_name = $3.strip!
+
+        extension = $4
+        
+        if (@ignored_directories.any?{ |obj| (file_name.index(obj) == 0) })
+          next
+        end
+        
+        language = get_language_for_file(file_name, commit_hash)
+
+        if language == nil
+          next
+        end
+
+        if (file_types.key? language.name.to_sym)
+          file_types[language.name.to_sym][:lines] += lines
+          file_types[language.name.to_sym][:edits] += edits
+        else
+          file_types[language.name.to_sym] = {
+            :lines => lines,
+            :edits => edits
+          }
+        end
+
+        total_lines += lines
+        total_edits += edits
+
+
+      end
+    end
+
+    return {
+        :lines => total_lines,
+        :edits => total_edits,
+        :file_types => file_types
+    }
+
+  end
+
+  def get_language_for_file(file_name, commit_hash)
+    escaped_file_name = file_name.gsub(/([\[\(\)\]\{\}\*\?\\])/, '\\\\\1')
+
+    full_file_path = File.join(Dir.home, @directory.gsub(/^~/, ""), file_name)
+
+    if File.file? full_file_path
+      language = LanguageSniffer.detect(full_file_path).language;
+    else 
+      language = LanguageSniffer.detect(full_file_path, :content => get_file(commit_hash, escaped_file_name), :path => full_file_path).language;
+    end
+
+    return language
+  end
+
+  def get_commit_details
+
+    puts "analyzing commits in #{@directory}".color(:yellow)
+    
+    bar = ProgressBar.new(@commits.length, :bar, :percentage, :eta);
+
+    @commits.each { |commit|
+      
+      bar.increment!
+
+      diff_tree = parse_diff_tree(commit[:hash])
+
+      commit.merge!(diff_tree)
+    }
+
+    return @commits
+
+  end
+
 end
